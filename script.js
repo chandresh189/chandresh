aa0bc34b33c5d76340241ee6b071048d7176dd7c
Chandresh Singh <48803866+chandresh189@users.noreply.github.com>
2021-06-04 19:09:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,24 @@
 var c = 0;
-document.querySelector(".c1").onclick = function () {
-    document.querySelector(".mail-type-container").classList.add('primary')
-    document.querySelectorAll(".mail-type-container")[1].classList.remove('social')
-    document.querySelectorAll(".mail-type-container")[2].classList.remove('promotion')
-    document.querySelectorAll(".mail-type-container span")[0].style.color = 'red'
-    document.querySelectorAll(".mail-type-container span")[1].style.color = 'gray'
-    document.querySelectorAll(".mail-type-container span")[2].style.color = 'gray'
+document.querySelector(".menu-btn").onclick = function () {
+	// if(document.querySelector("nav .menu").style.top === "0") {
+	// 	console.log("run if");
+	// 	document.querySelector("nav .menu").toggleClass
+	// }
+	// else if (document.querySelector("nav .menu").style.top === "-100%") {
+	// 	console.log("run else if");
+	// 	document.querySelector("nav .menu").style.top = "0";
+	// }
+	console.log(c)
+	if((c++) %2 != 0) {
+		document.querySelector("nav .menu").classList.add('active')
+	}
+	else {
+		document.querySelector("nav .menu").classList.remove('active')
+	}
 }
 
-document.querySelector(".c2").onclick = function () {
-    document.querySelector(".mail-type-container").classList.remove('primary')
-    document.querySelectorAll(".mail-type-container")[1].classList.add('social')
-    document.querySelectorAll(".mail-type-container")[2].classList.remove('promotion')
-    document.querySelectorAll(".mail-type-container span")[0].style.color = 'gray'
-    document.querySelectorAll(".mail-type-container span")[1].style.color = 'blue'
-    document.querySelectorAll(".mail-type-container span")[2].style.color = 'gray'
-}
-
-document.querySelector(".c3").onclick = function () {
-    document.querySelector(".mail-type-container").classList.remove('primary')
-    document.querySelectorAll(".mail-type-container")[1].classList.remove('social')
-    document.querySelectorAll(".mail-type-container")[2].classList.add('promotion')
-    document.querySelectorAll(".mail-type-container span")[0].style.color = 'gray'
-    document.querySelectorAll(".mail-type-container span")[1].style.color = 'gray'
-    document.querySelectorAll(".mail-type-container span")[2].style.color = 'green'
-}
-
-document.querySelector(".compose").onclick = function () {
-    document.querySelector(".send-mail").style.display='block'
-}
-
-
-
-function sendEmail() {
-
-    var data = $('form').serializeArray();
-    var from = "chetanyajangir9753@gmail.com";
-
-	Email.send({
-    SecureToken : "4ebfd2d8-f1c8-4b3b-9d3f-2ccb9eb8e743",
-    To : data[0].value,
-	From : from,
-	Subject : data[1].value,
-	Body : data[2].value,
-	}).then(
-		message => alert(message)
-	);
-    
-    document.querySelector(".send-mail").style.display = 'none'
-}
+// $(document).ready(function(){
+// 	$('.menu-btn').click(function() {
+// 		$('nav .menu').toggleClass("active");
+// 	})
+// });
